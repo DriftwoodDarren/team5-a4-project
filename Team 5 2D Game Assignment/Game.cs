4ebd7866ad71532c8a43fe767417aa4286af2d05
Darren Scott <000958343@mohawkcollege.ca>
2025-03-18 10:45:11 -0400

    Add Window Setup

--- Team 5 2D Game Assignment/Game.cs
+++ Team 5 2D Game Assignment/Game.cs
@@ -18,7 +18,9 @@ namespace MohawkGame2D
         /// </summary>
         public void Setup()
         {
-
+            Window.SetTitle("Dinosaur Game");
+            Window.SetSize(400, 400);
+            Window.ClearBackground(Color.OffWhite);
         }
 
         /// <summary>
