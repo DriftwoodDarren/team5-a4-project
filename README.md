89356e4719db67fd6b3ac41608eca74fe4bb75f3
Darren Scott <000958343@mohawkcollege.ca>
2025-03-31 12:55:04 -0400

    Add Background Image

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # team5-a4-project
 Collaborative game assignment for Game Development Fundamentals
+Alon - Platform, objects, collision, Audio
+Dylan - Player Controls, Score, Setup
--- Team 5 2D Game Assignment/Game.cs
+++ Team 5 2D Game Assignment/Game.cs
@@ -1,98 +1,100 @@
-﻿// Include the namespaces (code libraries) you need below.
-using System;
+﻿using System;
+using System.Data;
+using System.Linq.Expressions;
 using System.Numerics;
 using Team_5_2D_Game_Assignment;
 
-// The namespace your code is in.
 namespace MohawkGame2D
 {
-    /// <summary>
-    ///     Your game code goes inside this class!
-    /// </summary>
     public class Game
     {
-        // Place your variables here:
-        
-        // score variables
         public float score;
         public string scoreText;
 
+        // Setup Screen Variables
+        public bool runningGame;
+        Texture2D backGround = Graphics.LoadTexture("../ ../../../Assests/Images/start_screen.jpg");
+
         // class declarations
         private Player player;
+        private Obstacle obstacle;
+        private float platformHeight = 300; 
 
-        //Constructor
         public Game()
         {
-            player = new Player(new Vector2(50, 200)); // Initialize player at start position
-            //obstacle = new Obstacle(new Vector2(380, 200)); Use for obstacle constructor
+            player = new Player(new Vector2(50, platformHeight), platformHeight);
+            obstacle = new Obstacle(new Vector2(400, platformHeight));
         }
 
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
         public void Setup()
         {
-
-            Window.SetTitle("Dinosaur Game");
+            Window.SetTitle("Mohawk2D Dino Game");
             Window.SetSize(400, 400);
-            Draw.LineColor = Color.Black;
             Window.TargetFPS = 60;
+
+            ResetGameState();
 
         }
 
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
         public void Update()
         {
+            if (!runningGame)
+            {
+                Text.Color = Color.White;
+                Graphics.Draw(backGround, Window.Width / 2, Window.Height / 2);
+                Text.Draw("Press SpaceBar to Start!", 200, 200); 
+                
+                if (Input.IsKeyboardKeyPressed(KeyboardInput.Space))
+                {
+                    runningGame = true;
+                }
+            }
+            else
+            {
+                // Score updates
+                score += 1 * Time.DeltaTime;
+                int intScore = (int)score;
+                scoreText = intScore.ToString();
 
-            // Score updates
-            score += 1 * Time.DeltaTime;
-            int intScore = (int)score;
-            scoreText = intScore.ToString();
+                //Clear background
+                Window.ClearBackground(Color.OffWhite);
 
-            //Clear background
-            Window.ClearBackground(Color.OffWhite);
+                // Move player based on spacebar input
+                bool jumpPressed = Input.IsKeyboardKeyDown(KeyboardInput.Space);
+                player.Move(jumpPressed);
 
-            // Move player based on spacebar input
-            bool jumpPressed = Input.IsKeyboardKeyDown(KeyboardInput.Space);
-            player.Move(jumpPressed);
+                // Check for collision
+                //if (Collision.Check(player, obstacle))
+                // {
+                //    // Reset score on collision
+                //    score = 0;
+                // }
+                // ^ Use this spot for collision update call
 
-            // Check for collision
-            //if (Collision.Check(player, obstacle))
-            // {
-            //    // Reset score on collision
-            //    score = 0;
-            // }
-            // ^ Use this spot for collision update call
-
-            // Draw Elements
-            DrawPlayer();
-            DrawScore();
+                // Draw Elements
+                DrawPlayer();
+                DrawScore();
+            }
+            
         }
 
         public void DrawPlayer()
         {
-            // Draw the player
-            Draw.FillColor = Color.Red;
-            Draw.Rectangle(player.position.X, player.position.Y, 20, 20);
+            Draw.FillColor = Color.DarkGray;
+            Draw.Rectangle(0, platformHeight + 20, 400, 10);
         }
 
-        //public void DrawObstacle()
-        //{
-        //    // Draw the obstacle (box)
-        //    Draw.FillColor = Color.White;
-        //   Draw.Rectangle(obstacle.position.X, obstacle.position.Y, 20, 20);
-        //} ^^ Draw function for obstacle
-
-        public void DrawScore()
+        private void DrawScore()
         {
-            // Draw the score at the top of the screen
-            Draw.FillColor = Color.White;
-            Text.Draw("Score: ", 10, 10);
-            Text.Draw(scoreText, 120, 10);
+            Draw.FillColor = Color.Black;
+            Text.Draw("Score: " + scoreText, 10, 10);
         }
 
+        // Function for Resetting Game 
+        public void ResetGameState()
+        {
+            score = 0;
+            runningGame = false;
+        }
     }
-
 }
